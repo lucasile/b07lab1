ddcb7525c9002ee5b6f37f4bb17732b34609f1da
Lucas I <lucasi@Lucass-MacBook-Pro.local>
2022-09-30 12:17:42 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,16 +1,47 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Driver {
     public static void main(String [] args) {
-        Polynomial p = new Polynomial();
-        System.out.println(p.evaluate(3));
-        double [] c1 = {6,0,0,5};
-        Polynomial p1 = new Polynomial(c1);
-        double [] c2 = {0,-2,0,0,-9};
-        Polynomial p2 = new Polynomial(c2);
-        Polynomial s = p1.add(p2);
-        System.out.println("s(0.1) = " + s.evaluate(0.1));
-        if(s.hasRoot(1))
-            System.out.println("1 is a root of s");
-        else
-            System.out.println("1 is not a root of s");
+
+        //Polynomial p1 = new Polynomial(new double[]{5, -2, 3, 1}, new int[]{0, 2, 3, 4});
+        //printPolynomial(p1);
+        //System.out.println("\n\n");
+
+        //Polynomial p2 = new Polynomial(new double[]{2, 1, -6, 1}, new int[]{0, 2, 3, 5});
+
+        //Polynomial p1plusp2 = p1.add(p2);
+
+        //printPolynomial(p1plusp2);
+
+        //Polynomial p1timesp2 = p1.multiply(p2);
+
+        //printPolynomial(p1timesp2);
+
+        //try {
+            //p1timesp2.saveToFile("test.txt");
+        //} catch (IOException e) {
+        //    e.printStackTrace();
+        //}
+
+        File polynomialFile = new File("./polynomial.txt");
+
+        try {
+            Polynomial importedPolynomial = new Polynomial(polynomialFile);
+            printPolynomial(importedPolynomial);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
     }
+
+    private static void printPolynomial(Polynomial polynomial) {
+        for (int i = 0; i < polynomial.getExponents().length; i++) {
+            System.out.println(polynomial.getCoefficients()[i] + "x^" + polynomial.getExponents()[i]);
+        }
+    }
+
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,34 +1,112 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
+
 public class Polynomial {
 
     private double[] coefficients;
+    private int[] exponents;
 
     public Polynomial() {
         coefficients = new double[] {0.0};
+        exponents = new int[] {0};
     }
 
-    public Polynomial(double[] coefficients) {
+    public Polynomial(double[] coefficients, int[] exponents) {
         this.coefficients = coefficients.clone();
+        this.exponents = exponents.clone();
+    }
+
+    public Polynomial(File file) throws FileNotFoundException {
+
+        Scanner scanner = new Scanner(file);
+
+        String polynomialString = null;
+
+        if (scanner.hasNextLine())
+            polynomialString = scanner.nextLine();
+
+        scanner.close();
+
+        if (polynomialString == null) {
+            coefficients = new double[]{0.0};
+            exponents = new int[] {0};
+            return;
+        }
+
+        createFromString(polynomialString);
+
     }
 
     public Polynomial add(Polynomial polynomial) {
 
+        Map<Integer, Double> expToCoeff = new HashMap<>();
+
+        int[] otherExponents = polynomial.getExponents();
         double[] otherCoefficients = polynomial.getCoefficients();
 
-        double[] newCoefficients;
-        double[] toAdd;
-
-        if (otherCoefficients.length > coefficients.length) {
-            newCoefficients = otherCoefficients.clone();
-            toAdd = coefficients;
-        } else {
-            newCoefficients = coefficients.clone();
-            toAdd = otherCoefficients;
+        for (int i = 0; i < otherExponents.length; i++) {
+            if (expToCoeff.containsKey(otherExponents[i]))
+                expToCoeff.put(otherExponents[i], expToCoeff.get(otherExponents[i]) + otherCoefficients[i]);
+            else
+                expToCoeff.put(otherExponents[i], otherCoefficients[i]);
         }
 
-        for (int i = 0; i < toAdd.length; i++)
-            newCoefficients[i] += toAdd[i];
+        for (int i = 0; i < exponents.length; i++) {
+            if (expToCoeff.containsKey(exponents[i]))
+                expToCoeff.put(exponents[i], expToCoeff.get(exponents[i]) + coefficients[i]);
+            else
+                expToCoeff.put(exponents[i], coefficients[i]);
+        }
 
-        return new Polynomial(newCoefficients);
+        return createPolynomialFromMap(expToCoeff);
+    }
+
+    public Polynomial multiply(Polynomial polynomial) {
+
+        Map<Integer, Double> expToCoeff = new HashMap<>();
+
+        double[] otherCoefficients = polynomial.getCoefficients();
+        int[] otherExponents = polynomial.getExponents();
+
+        for (int i = 0; i < exponents.length; i++) {
+
+            double coefficient = coefficients[i];
+            int exponent = exponents[i];
+
+            for (int j = 0; j < otherExponents.length; j++) {
+
+                double otherCoefficient = otherCoefficients[j];
+                int otherExponent = otherExponents[j];
+
+                double newCoefficient = coefficient * otherCoefficient;
+                int newExponent = exponent + otherExponent;
+
+                if (expToCoeff.containsKey(newExponent))
+                    expToCoeff.put(newExponent, expToCoeff.get(newExponent) + newCoefficient);
+                else
+                    expToCoeff.put(newExponent, newCoefficient);
+
+            }
+
+        }
+
+        // purge zeroes from the map
+        // would use iterator, but we haven't learned that yet
+
+        List<Integer> keysToRemove = new ArrayList<>();
+
+        for (Map.Entry<Integer, Double> entry : expToCoeff.entrySet()) {
+            if (entry.getValue() == 0)
+                keysToRemove.add(entry.getKey());
+        }
+
+        for (Integer key : keysToRemove)
+            expToCoeff.remove(key);
+
+        return createPolynomialFromMap(expToCoeff);
     }
 
     public double evaluate(double input) {
@@ -36,13 +114,129 @@ public class Polynomial {
         double result = 0.0;
 
         for (int i = 0; i < coefficients.length; i++)
-            result += coefficients[i] * Math.pow(input, i);
+            result += coefficients[i] * Math.pow(input, exponents[i]);
 
         return result;
     }
 
     public boolean hasRoot(double input) {
         return evaluate(input) == 0.0;
+    }
+
+    public void saveToFile(String fileName) throws IOException {
+
+        StringBuilder polynomialString = new StringBuilder();
+
+        for (int i = 0; i < coefficients.length; i++) {
+
+            double coefficient = coefficients[i];
+            int exponent = exponents[i];
+
+            if (coefficient != 1)
+                polynomialString.append(coefficient);
+
+            if (exponent != 0) {
+                polynomialString.append("x");
+                if (exponent != 1)
+                    polynomialString.append(exponent);
+            }
+
+            if (i + 1 != coefficients.length && coefficients[i + 1] > 0)
+                polynomialString.append("+");
+
+        }
+
+        File file = new File(fileName);
+
+        // will create new file iff it doesn't exist... don't care about the return type
+        file.createNewFile();
+
+        FileWriter fileWriter = new FileWriter(file, false);
+
+        fileWriter.write(polynomialString.toString());
+        fileWriter.close();
+
+    }
+
+    private void createFromString(String polynomialString) {
+
+        List<String> terms = new ArrayList<>();
+        char[] stringCharacters = polynomialString.toCharArray();
+
+        int i = 1;
+        int length = stringCharacters.length;
+
+        // I would use regex, but we haven't learned that in class
+
+        StringBuilder lastTerm = new StringBuilder(String.valueOf(stringCharacters[0]));
+
+        while (i < length) {
+
+            char character = stringCharacters[i];
+
+            if (character == '+' || character == '-' || i + 1 == length) {
+                if (i + 1 == length)
+                    lastTerm.append(character);
+                terms.add(lastTerm.toString());
+                lastTerm = new StringBuilder(String.valueOf(character));
+                i++;
+                continue;
+            }
+
+            lastTerm.append(character);
+
+            i++;
+        }
+
+        coefficients = new double[terms.size()];
+        exponents = new int[terms.size()];
+
+        for (int j = 0; j < terms.size(); j++) {
+
+            String term = terms.get(j);
+
+            String sanitized = term.replaceAll("\\+", "");
+            String[] splitTerm = sanitized.split("x");
+
+            // first term is x, then first element in array will be ""
+            if (splitTerm[0].equals(""))
+                coefficients[j] = 1.0;
+            else
+                coefficients[j] = Double.parseDouble(splitTerm[0]);
+
+            // then we have some coefficient followed by just x, like 5x, or just 5 -> default exponent to 1
+            if (splitTerm.length == 1) {
+                if (splitTerm[0].equals(sanitized))
+                    exponents[j] = 0;
+                else
+                    exponents[j] = 1;
+            } else
+                exponents[j] = Integer.parseInt(splitTerm[1]);
+
+        }
+
+    }
+
+    private Polynomial createPolynomialFromMap(Map<Integer, Double> map) {
+
+        int size = map.size();
+
+        int[] newExponents = new int[size];
+        double[] newCoefficients = new double[size];
+
+        int i = 0;
+        for (int exponentKey : map.keySet()) {
+            newExponents[i] = exponentKey;
+            newCoefficients[i] = map.get(exponentKey);
+            i++;
+        }
+
+        return new Polynomial(newCoefficients, newExponents);
+
+    }
+
+    public int[] getExponents() {
+        return exponents;
     }
 
     public double[] getCoefficients() {
